4655f01ad9d00f3868d0d45ea24d9e31a19400dd
codywohlers <code@codywohlers.ca>
2025-01-03 11:32:04 -0700

    changed script argument to not need https

--- scripts/create-dir-entry.sh
+++ scripts/create-dir-entry.sh
@@ -7,6 +7,7 @@
 # - get country
 # - output html
 
+# 2025-Jan-03 code@codwyohlers.ca - changed argument to not need https
 # 2025-Jan-01 code@codwyohlers.ca - initial creation.
 
 
@@ -20,8 +21,11 @@ NAME="$2"
 cd ~/tmp
 
 # check if valid URL (from https://stackoverflow.com/a/3184819/3394887)
-regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
-if [[ ! "$URL" =~ $regex ]] ;then echo "Error: Invalid URL: $URL" ;exit 1 ;fi
+#regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
+#if [[ ! "$URL" =~ $regex ]] ;then echo "Error: Invalid URL: $URL" ;exit 1 ;fi
+
+# remove http(s):// and trailing / if present
+URL=`echo "$URL" |sed 's|^https*://||' |sed 's|/$||'`
 
 # fake the user-agent so gay bot detection doesn't be gay
 #USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/20100101 Firefox/100.0'
@@ -29,30 +33,33 @@ if [[ ! "$URL" =~ $regex ]] ;then echo "Error: Invalid URL: $URL" ;exit 1 ;fi
 
 # use headless chrome to get a rendered dom html.
 #DATA=$(google-chrome --user-agent="$USER_AGENT" --incognito --headless --dump-dom "$URL" 2>/dev/null)
-DATA=$(google-chrome --incognito --headless --dump-dom "$URL" 2>/dev/null)
+DATA=$(google-chrome --incognito --headless --dump-dom "https://""$URL" 2>/dev/null)
 # Use for testing with a file as an argument:
 #DATA="$(cat "$1")"
 
 
 # get favicon and save as useful filename.
-FAVICON_URL="$URL"`echo "$DATA" |grep link |grep \"icon\" |head -n1 |grep -oe href=\".*\" |sed 's/"$//' |sed 's/^href="//'`
+FAVICON_URL="https://""$URL"`echo "$DATA" |grep link |grep \"icon\" |head -n1 |grep -oe href=\".*\" |sed 's/"$//' |sed 's/^href="//'`
 echo "favicon $FAVICON_URL"
 EXT=`echo $FAVICON_URL |sed 's/.*\.//'`
-DOMAIN=`echo "$URL" |sed 's|^https*://||' |sed 's|/$||'`
-#curl -o  $DOMAIN.$EXT "$FAVICON_URL"
-wget -nv -O $DOMAIN.$EXT "$FAVICON_URL"
-mv -v $DOMAIN.$EXT "$HOME/Projects/dir.wc.ca/html/img/"
+if [ "$EXT" == "png" ] || [ "$EXT" == "ico" ] ;then
+	#curl -o  $URL.$EXT "$FAVICON_URL"
+	wget -nv -O $URL.$EXT "$FAVICON_URL"
+	mv -v $URL.$EXT "$HOME/Projects/dir.wc.ca/html/img/"
+else
+	echo "favicon extension unknown, not downloading" >&2
+fi
 
 
-COUNTRY_CODE=`whois "$DOMAIN" |grep -im1 country | grep -o [A-Z][A-Z]$`
+COUNTRY_CODE=`whois "$URL" |grep -im1 country | grep -o [A-Z][A-Z]$`
 #COUNTRY_CODE="US"
 
 COUNTRY_NAME=`cat "/media/mammoth/library/data/country-code-names.txt" |grep "^$COUNTRY_CODE" |sed "s/^$COUNTRY_CODE //"`
 COUNTRY_FLAG=`cat "/media/mammoth/library/data/country-code-flags.txt" |grep "^$COUNTRY_CODE" |sed "s/^$COUNTRY_CODE //"`
 
 
-OUT="<tr><td class=\"center\"><a href=\"$URL\"><img src=\"../../img/$DOMAIN.$EXT\" height=50></a> </td>
-    <td><a href=\"$URL\">$NAME</a></td>
+OUT="<tr><td class=\"center\"><a href=\"https://$URL\"><img src=\"../../img/$DOMAIN.$EXT\" height=50></a> </td>
+    <td><a href=\"https://$URL\">$NAME</a></td>
 	<td class=\"small-font left-pad\"><span title=\"$COUNTRY_NAME\">$COUNTRY_FLAG</span> $DOMAIN</td>
 </tr>"
 
